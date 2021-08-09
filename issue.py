d87a537f5e963996b3dc1237c287517dab125367
pwaycuilis <85208803+pwaycuilis@users.noreply.github.com>
2021-08-08 22:25:17 -0500

    Update issue.py

--- issue.py
+++ issue.py
@@ -1,4 +1,6 @@
-
+#The issue unit follows the basic scoreboarding algorithm to issue instructions. It can issue up to two instructions, out of order, per clock cycle.
+#When an instruction is issued, it moves out of the pre-issue buffer and into either the pre-mem buffer or the pre-ALU buffer. 
+#The issue unit searches from entry 0 to entry 3 (in that order) of the pre-issue buffer.
 class Issue:
 
     memIndexOfNext = 96
@@ -27,34 +29,50 @@ class Issue:
     def run(self):
 
         numInIssueAtClockCycleBegin = 0
-
-        # print(f"preIssueBuff in issue{self.preIssueBuff}")
+        ##do iso loop first to determine this, then while pre, post diff is < 2, keep going (while loop)
         for i in range(len(self.preIssueBuff)):
-            if self.preIssueBuff[i] != -1:          #if preIssueBuff has valid instr
+            if self.preIssueBuff[i] != -1:
                 numInIssueAtClockCycleBegin += 1
+                print(f"(issue34)numInIssue = {numInIssueAtClockCycleBegin}")
 
-                # print (f"numInIssue unit{numInIssueAtClockCycleBegin}")
+
+            ##if current index instr = "ADDI" (or some other valid instr)
+            ## and if preALUBuff[0] is empty and preissuebuff[i] == currIndex:
+            ##   move contents from preIssueBuff[i] to preALUBuff[0], clear preIssueBuff[i] and currAddr++
+            ##elif preALUBuff[1] is empty and preIssueBuff[i] = currIndex:
+            ##   move contents from preIssueBuff[i] to preALUBuff[1], clear preIssueBuff[i] and currAddr++
 
                 if self.opcodeStr[self.preIssueBuff[i]] == "ADDI" or self.opcodeStr[self.preIssueBuff[i]] == "ADD"\
                         or self.opcodeStr[self.preIssueBuff[i]] == "SUB" or self.opcodeStr[self.preIssueBuff[i]] == "SUBI"\
                         or self.opcodeStr[self.preIssueBuff[i]] == "AND" or self.opcodeStr[self.preIssueBuff[i]] == "ORR"\
-                        or self.opcodeStr[self.preIssueBuff[i]] == "EOR":
-                    if self.preALUBuff[0] == -1 and self.preIssueBuff[i] == self.currIndex:# and self.address[i] == self.memIndexOfNext:            #and if preALUBuff has open slot
+                        or self.opcodeStr[self.preIssueBuff[i]] == "EOR" or self.opcodeStr[self.preIssueBuff[i]] == "LSL":
+                    if self.preALUBuff[0] == -1 and self.preIssueBuff[i] == self.currIndex:# and self.address[i] == self.memIndexOfNext:    #and if preALUBuff has open slot
                         self.preALUBuff[0] = self.preIssueBuff[i]
                         self.preIssueBuff[i] = -1           #set preissuebuff back to -1
                         self.currIndex += 1
-                        # self.memIndexOfNext += 4
+                        ## self.memIndexOfNext += 4
 
                     elif self.preALUBuff[1] == -1 and self.preIssueBuff[i] == self.currIndex:# and self.address[i] == self.memIndexOfNext:
                         self.preALUBuff[1] = self.preIssueBuff[i]
                         self.preIssueBuff[i] = -1       #set preissuebuff back to -1
                         self.currIndex += 1
-                        # self.memIndexOfNext += 4
+                        ## self.memIndexOfNext += 4
+
+                ## for STUR/LDUR instructions
+                if self.opcodeStr[self.preIssueBuff[i]] == "STUR":
+                    if self.preMemBuff[0] == -1 and self.preIssueBuff[i] == self.currIndex:
+                        self.preMemBuff[0] = self.preIssueBuff[i]
+                        self.preIssueBuff[i] = -1
+                        self.currIndex += 1
+
+                    elif self.preMemBuff[1] == -1 and self.preIssueBuff[i] == self.currIndex:
+                        self.preMemBuff[1] = self.preIssueBuff[i]
+                        self.preIssueBuff[i] = -1
+                        self.currIndex += 1
+                ###***
+                #if self.opcodedStr[self.preIssueBuff[i]]
 
 
-        #print(f"num at end{numInIssueAtClockCycleBegin}")
-
-        # print(f"pre")
 
         # return Issue(self.preIssueBuff, self.preMemBuff, self.preALUBuff, self.instruction, self.opcode, self.opcodeStr,
         #          self.dataval, self.address, self.arg1, self.arg2, self.arg3, self.numInstructions, self.destReg, self.src1Reg, self.src2Reg)
